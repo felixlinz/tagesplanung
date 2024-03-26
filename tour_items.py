d4461b50334d804f67a0b0b3dccbf0092aaa1d54
Felix Linz <felix.linz@icloud.com>
2024-03-26 01:52:23 +0100

    gn fucked up

--- tour_items.py
+++ tour_items.py
@@ -25,18 +25,17 @@ class ToggleEntryCombo:
         
 
 class TourItem:
-    def __init__(self, master_list, number, wave, alt_time, index):
+    def __init__(self, master_list, number, wave, alt_time):
         self.uberframe = ttk.Frame(master_list.frame)
-        self.index = index
         self.parent = master_list
         self.tour_number = number
         self.frame = ttk.Frame(self.uberframe, height="64", width= "256")
         self.tour_label = ttk.Label(self.frame, text=str(self.tour_number))
-        self.delete_button = ttk.Button(self.frame, text="x", command=self.self_delete)
+        self.delete_button = ttk.Button(self.frame, text="x", width="1", command=self.self_delete)
         self.tour_label.config(font=("Myriad Pro", "18"))
         self.toggle_combo = ToggleEntryCombo(self.frame, int(wave)-1, alt_time )
-        self.tour_label.pack(side="left", pady="12")
-        self.toggle_combo.frame.pack(side="left", pady="12")
+        self.tour_label.pack(side="left", pady="4", padx="8")
+        self.toggle_combo.frame.pack(side="left", pady="4", padx="8")
         self.delete_button.pack(side="left", padx="4")
         self.frame.pack(side="top", padx="16")
         separator = ttk.Separator(self.uberframe, orient='horizontal')
@@ -54,6 +53,41 @@ class TourItem:
             alt_time = ""
         return (self.tour_number, wave, alt_time)
                 
+        
+class EntryTourItem:
+    def __init__(self, master, command, number ="001"):
+        self.command = command
+        self.uberframe = ttk.Frame(master)
+        self.frame = ttk.Frame(self.uberframe, height="64", width= "256")
+        self.tour_number_variable = tk.StringVar(value=number)
+        self.tour_number = ttk.Entry(self.frame, width="3", textvariable=self.tour_number_variable)
+        self.tour_number.config(font=("Myriad Pro", "12"))
+        self.add_button = ttk.Button(self.frame, text="+", command=lambda: self.command(self.return_values()))
+        self.toggle_combo = ToggleEntryCombo(self.frame, 0, "00:00" )
+        self.add_button.pack(side="left", padx="8")
+        self.tour_number.pack(side="left", pady="12")
+        self.toggle_combo.frame.pack(side="left", pady="12")
+        self.frame.pack(side="top", padx="16")
+        separator = ttk.Separator(self.uberframe, orient='horizontal')
+        separator.pack(side="bottom", fill='x')
+        
+        
+    def self_add(self):
+        if self.toggle_combo.toggle_button.return_state() == 2:
+            alt_time = self.toggle_combo.alt_time
+        else:
+            alt_time = ""
+        self.list.touren.add(TourItem(self.list, self.tour_number.get(), self.toggle_combo.return_state() + 1, alt_time))
+        self.uberframe.destroy()
+        
+        
+    def return_values(self):
+        wave = self.toggle_combo.return_state() + 1
+        if self.toggle_combo.toggle_button.return_state() == 2:
+            alt_time = self.toggle_combo.alt_time
+        else:
+            alt_time = ""
+        return (self.tour_number.get(), self.toggle_combo.return_state() +1, alt_time, self)   
                 
 class TourList:
     def __init__(self, master, tourlist):
@@ -62,11 +96,16 @@ class TourList:
         
         for n, dataset in enumerate(tourlist):
             tour_number, wave_number, alt_time = dataset
-            item = TourItem(self, tour_number, wave_number, alt_time, n)
+            item = TourItem(self, tour_number, wave_number, alt_time)
             self.touren.append(item)
         
+        self.refresh()
+            
+    def refresh(self):
+        self.touren.sort(key=lambda x: int(x.tour_number))
         for item in self.touren:
             item.uberframe.pack(side="top")
+            
             
     def get_updated_values(self):
         updated_list = []
@@ -75,3 +114,26 @@ class TourList:
         return updated_list
         
 
+class TourList2:
+    def __init__(self, master, tourlist):
+        self.frame = tk.Canvas(master, height="400")      
+        self.touren = []
+        
+        for n, dataset in enumerate(tourlist):
+            tour_number, wave_number, alt_time = dataset
+            item = TourItem(self, tour_number, wave_number, alt_time)
+            self.touren.append(item)
+        
+        self.refresh()
+            
+    def refresh(self):
+        self.touren.sort(key=lambda x: int(x.tour_number))
+        for item in self.touren:
+            item.uberframe.pack(side="top")
+            
+            
+    def get_updated_values(self):
+        updated_list = []
+        for item in self.touren:
+            updated_list.append(item.return_values())
+        return updated_list
