73263e2e465cf6fb2bc4c66d8f97b6e5c8a36c65
Felix Linz <felix.linz@icloud.com>
2024-03-29 00:27:49 +0100

    "next up, main bit"

--- playground.py
+++ playground.py
@@ -1,10 +1,35 @@
 # import tkinter as tk
+from datetime import datetime
+from datetime import date
+import pandas as pd
 import ttkbootstrap as ttk
-from times_manager import DailyTimesManager, FilledDayTimeAdjuster
+from data_manager import learn_data_from_excel, sort_workers, DailyToursManager
+from tour_items import TagesplanungTourList
 
+date_string = "2023-03-28"  # Your date string here
+date_format = "%Y-%m-%d"  # The format of your date string
+
+date_obj = datetime.strptime(date_string, date_format).date()
+
+dfs = sort_workers(learn_data_from_excel(date.today()))
 
 master = ttk.Frame()
-datamanager = DailyTimesManager(master)
-datamanager.frame.pack()
+datamanager = DailyToursManager()
+day_data = datamanager.read_data("tuesday")
+dataeditor = TagesplanungTourList(master, day_data, dfs)
+dataeditor.frame.pack()
 master.pack()
 master.mainloop()
+
+
+"""print("Stammfahrer")
+print(dfs["Stammfahrer"])
+print("Springer")
+print(dfs["Springer"])
+print("Firmen")
+print(dfs["Firmen"])
+print("Einweisung")
+print(dfs["Einweisung"])
+print("Abrufer")
+print(dfs["Abrufer"])"""
+
--- tour_items.py
+++ tour_items.py
@@ -163,6 +163,64 @@ class TourList2:
                 self.touren.sort(key=lambda x: int(x.tour_number))
                 self.touren[-1].uberframe.pack(side="top")
                 
+                
+class TagesplanungTourList:
+    def __init__(self, master, tourlist, tagesplanung):
+        self.tourlist = tourlist # [(tourstring, wave, alt_time)]
+        self.tagesplanung = tagesplanung
+        self.stammfahrer = self.tagesplanung["Stammfahrer"]
+        self.springer = self.tagesplanung["Springer"]
+        self.abrufer = self.tagesplanung["Abrufer"]
+        self.firmenzusteller = self.tagesplanung["Firmen"]
+        self.Einweisung = self.tagesplanung["Einweisung"]
+        self.frame = tk.Canvas(master, height="400")      
+        self.touren = []
+        self.tour_numbers = set()
+        
+        for dataset in self.tourlist:
+            tour_number, wave_number, alt_time = dataset
+            filtered_df = self.stammfahrer[self.stammfahrer['Stammtour'] == int(tour_number)]
+            item = DriverTourItem(self, filtered_df["Name"], tour_number, wave_number, alt_time)
+            self.tour_numbers.add(int(tour_number))
+            self.touren.append(item)
+        
+        for item in self.touren:
+            item.uberframe.pack(side="top")
+        
+        self.refresh()
+              
+    def refresh(self):
+        # Sort the list first
+        self.touren.sort(key=lambda x: int(x.tour_number))
+        
+        # Remove duplicates: Create a new list without duplicates
+        unique_touren = []
+        seen_numbers = set()
+        for item in self.touren:
+            if item.tour_number not in seen_numbers:
+                unique_touren.append(item)
+                seen_numbers.add(item.tour_number)
+        
+        # Now unique_touren has all unique items, sorted by tour_number
+        self.touren = unique_touren
+        
+        # Clear the existing items from the GUI
+        for item in self.frame.winfo_children():
+            item.pack_forget()
+        
+        # Repack the items in self.touren
+        for item in self.touren:
+            item.uberframe.pack(side="top")
+            
+    def add_tour(self, tour_number, wave, alt_time):
+        if tour_number not in self.tour_numbers:
+            if int(tour_number) > max(self.tour_numbers):
+                self.touren.append(TourItem(self, tour_number, wave, alt_time))
+                self.refresh()
+            else:
+                self.touren.sort(key=lambda x: int(x.tour_number))
+                self.touren[-1].uberframe.pack(side="top")
+                
         
             
     def get_updated_values(self):
@@ -170,3 +228,37 @@ class TourList2:
         for item in self.touren:
             updated_list.append(item.return_values())
         return updated_list
+    
+    
+class DriverTourItem:
+    def __init__(self, master_list, driver, number, wave, alt_time = ""):
+        self.uberframe = ttk.Frame(master_list.frame)
+        self.parent = master_list
+        self.driver = driver
+        self.tour_number = number
+        self.frame = ttk.Frame(self.uberframe, height="64", width= "256")
+        self.tour_label = ttk.Label(self.frame, text=str(self.tour_number))
+        self.driver_variable = tk.StringVar(value=self.driver)
+        self.driver_entry = ttk.Entry(self.frame, textvariable=self.driver_variable, width="8")
+        self.delete_button = ttk.Button(self.frame, text="x", width="1", command=self.self_delete)
+        self.tour_label.config(font=("Myriad Pro", "18"))
+        self.toggle_combo = ToggleEntryCombo(self.frame, int(wave)-1, alt_time )
+        self.tour_label.pack(side="left", pady="4", padx="8")
+        self.driver_entry.pack(side="left", pady="4", padx="8")
+        self.toggle_combo.frame.pack(side="left", pady="4", padx="8")
+        self.delete_button.pack(side="left", padx="4")
+        self.frame.pack(side="top", padx="16")
+        separator = ttk.Separator(self.uberframe, orient='horizontal')
+        separator.pack(side="bottom", fill='x')
+        
+    def self_delete(self):
+        self.parent.touren.remove(self)
+        self.uberframe.destroy()
+        
+    def return_values(self):
+        wave = self.toggle_combo.return_state() + 1
+        if self.toggle_combo.toggle_button.return_state() == 2:
+            alt_time = self.toggle_combo.alt_time
+        else:
+            alt_time = ""
+        return (self.tour_number,self.driver_variable.get(), wave, alt_time)
